f578139ecc6283f690c436652f90b11f870a5197
ankit <ankityadav8299blp@gmail.com>
2026-02-13 12:53:44 +0530

    added motion framer animations to navbar, hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,69 @@
 import React from "react";
 import assets from "../assets/assets";
-
+import {motion} from "motion/react"
 const Hero = () => {
   return (
     <div
       id="hero"
       className="flex flex-col items-center gap-6 py-20 pz-4 sm:px-12 lg:px-24 xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text-white"
     >
-      <div className="inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        viewport={{ once: true }}
+        className="inline-flex items-center gap-2 border border-gray-300 p-1.5 pr-4 rounded-full"
+      >
         <img className="w-20" src={assets.group_profile} alt="" />
         <p className="text-xs font-medium">Trusted By 10k+ users</p>
-      </div>
+      </motion.div>
 
-      <h1 className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl">
+      <motion.h1
+        /* this is framer motion to add animations in the h1 tag */
+        /* initial tells what will be the initial condition of the element */
+        initial={{ opacity: 0, y: 40 }}
+        // this tells when the element is viewed
+        whileInView={{ opacity: 1, y: 0 }}
+        // how will there be change from initial -> view
+        transition={{ duration: 0.6, delay: 0.8 }}
+        // view port means whenever the element comes in front, repeat the transition
+        viewport={{ once: true }}
+        className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl"
+      >
         Turning imagination into{" "}
         <span className="bg-linear-to-r from-[#5044E5] to-[#4d8CEA] bg-clip-text text-transparent">
           digital
         </span>{" "}
         impact.
-      </h1>
+      </motion.h1>
 
-      <p className="text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3">
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        viewport={{ once: true }}
+        className="text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 max-w-4/5 sm:max-w-lg pb-3"
+      >
         Creating meaningful connections and turning big ideas into interactive
         digital experiences.
-      </p>
+      </motion.p>
 
-      <div className="relative">
-        <img src={assets.hero_img} alt="" className="w-full max-w-6xl"/>
-        <img src={assets.bgImage1} alt="" className="absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden"/>
-      </div>
+      <motion.div
+        initial={{ opacity: 0, scale:0.9 }}
+        whileInView={{ opacity: 1,scale:1 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+        viewport={{ once: true }}
 
+        
+        className="relative"
+      >
+        <img src={assets.hero_img} alt="" className="w-full max-w-6xl" />
+        <img
+          src={assets.bgImage1}
+          alt=""
+          className="absolute -top-40 -right-40 sm:-top-100 sm:-right-70 -z-1 dark:hidden"
+        />
+      </motion.div>
     </div>
   );
 };
